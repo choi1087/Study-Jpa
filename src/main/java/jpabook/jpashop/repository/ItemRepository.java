fbd48a0b7a1258918ae909dbc3c5df902a692b74
choi1087 <adcdm87@gmail.com>
2022-02-27 22:43:41 +0900

    item domain 개발

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item); //기존에 존재하지 않으므로 신규 등록
+        } else {
+            em.merge(item); //기존에 id가 존재하기 때문에(이미 db에 등록된 것) update 과 비슷한 개념
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
